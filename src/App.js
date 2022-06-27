b843342cee6280f3705313b30c746611fc03307b
wonma <wonmadesign@gmail.com>
2022-06-27 12:47:46 -0500

    Replaced static robots object with JSON placeholder

--- src/App.js
+++ src/App.js
@@ -2,28 +2,33 @@ import React, { Component } from 'react';
 import Cardlist from './Cardlist';
 import Searchbar from './Searchbar';
 import Scroll from './Scroll';
-import { robots } from './robots';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
+      robots: [],
       searchValue: ''
     };
   }
 
   filteredRobots = () => {
+    const { robots, searchValue } = this.state;
     return robots.filter(robot => {
-      return robot.name
-        .toLowerCase()
-        .includes(this.state.searchValue.toLowerCase());
+      return robot.name.toLowerCase().includes(searchValue.toLowerCase());
     });
   };
 
   onSearchChange = event => {
     this.setState({ searchValue: event.target.value });
   };
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }));
+  }
 
   render() {
     return (
