2043041808e5723a487e7b83967edd34436f1cd1
wonma <wonmadesign@gmail.com>
2022-06-26 22:00:39 -0500

    Initialized React App and Card Component

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    <div>
+      <img alt='robots' src='https://robohash.org/hehehe?200x200' />
+      <div>
+        <h2>Wonmi</h2>
+        <p>wonmadesign@gmail.com</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
