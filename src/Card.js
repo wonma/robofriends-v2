a200f13d8931c5ab94e836a7ca0b69275a9bb05a
wonma <wonmadesign@gmail.com>
2022-06-26 23:53:14 -0500

    Styled Card comp

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import './Card.css';
 
-const Card = () => {
+const Card = ({ id, name, username, email }) => {
   return (
-    <div>
-      <img alt='robots' src='https://robohash.org/hehehe?200x200' />
+    <div className='bg-light-blue fl br3 tc sans-serif ma2 pa3'>
+      <img
+        className='robot-wrap br-100 bg-dark-blue'
+        alt='robots'
+        src={`https://robohash.org/${id}?size=200x200`}
+      />
       <div>
-        <h2>Wonmi</h2>
-        <p>wonmadesign@gmail.com</p>
+        <h2>{name}</h2>
+        <p>{email}</p>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'tachyons';
 import Card from './Card';
+import { robots } from './robots';
 
-ReactDOM.render(<Card />, document.getElementById('root'));
+ReactDOM.render(
+  <div>
+    <Card name={robots[0].name} id={robots[0].id} email={robots[0].email} />
+    <Card name={robots[1].name} id={robots[1].id} email={robots[1].email} />
+    <Card name={robots[2].name} id={robots[2].id} email={robots[2].email} />
+  </div>,
+  document.getElementById('root')
+);
