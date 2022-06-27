86a940f36c67b50646a308f7d7aa1ea04e26b3e6
wonma <wonmadesign@gmail.com>
2022-06-27 14:38:02 -0500

    Updates

---
+++ precache-manifest.64af2b749d37af984334440b1031d355.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f90e2bcceec3511d812",
+    "url": "/static/css/main.a676987f.chunk.css"
+  },
+  {
+    "revision": "1f90e2bcceec3511d812",
+    "url": "/static/js/main.4cf7276d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "936744deb29368a73caa",
+    "url": "/static/css/2.016d9fc8.chunk.css"
+  },
+  {
+    "revision": "936744deb29368a73caa",
+    "url": "/static/js/2.a57b84b1.chunk.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/static/media/SEGA.82e09f78.TTF"
+  },
+  {
+    "revision": "5cde6f357f60bd6a14763dc14b945ce6",
+    "url": "/static/media/Loader.5cde6f35.svg"
+  },
+  {
+    "revision": "5328a200b257f306808bc06c1c145aba",
+    "url": "/index.html"
+  }
+];
